c0644f5527d64869f90350754b6b5ef7c86ef309
molestador <hustlerxxl@gmail.com>
2018-03-01 19:08:10 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+
+  let str2 = str.split('');
+  let stack = [];
+
+  for (let i = 0; i < str2.length; i += 1) {
+    for (let j = 0; j < bracketsConfig.length; j += 1) {
+      if (bracketsConfig[j][0] === str2[i]) {
+        stack.push(str2[i]);
+      }
+      if (bracketsConfig[j][1] === str2[i] && bracketsConfig[j][0] === stack[stack.length - 1]) {
+        stack.pop()
+      }
+    }
+  }
+  return stack.length === 0;
 }
+
